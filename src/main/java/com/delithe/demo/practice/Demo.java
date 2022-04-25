cc9b17352ce0d2442191bf14ceb7c178e1860b19
Archanapujar67 <archanapujar67@gmail.com>
2022-04-25 17:56:16 +0530

    This is for practice getting understand the how we need commit and push the code in GITHUB

---
+++ src/main/java/com/delithe/demo/practice/Demo.java
@@ -0,0 +1,20 @@
+package com.delithe.demo.practice;
+/*
+This Demo class will access to all the members of Dlithe organisation
+      @author:Archana
+     @Date:25-Apr-2022
+
+ */
+public class Demo {
+
+    /*The name of the addTwoNumbers methods will take 2 numbers
+    */
+    public  void  addTwoNumbers(int x,int y)
+
+    {
+
+        x=20;
+        y=23;
+        System.out.println(x+y);
+    }
+}
